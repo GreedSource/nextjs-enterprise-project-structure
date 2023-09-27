3427d26040d7488af79a5743e4a69741709489cf
Joel Garcia <joelgarciia95@gmail.com>
2023-09-26 23:15:50 -0600

    Initial config

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@components/test/Card';
+
+export default function Page() {
+  return (
+    <Card
+      title="Documentation"
+      body="Find in-depth information about Next.js features and API."
+      href="https://nextjs.org/docs"
+    />
+  );
+}
---
+++ src/common/components/test/Card/Card.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import styles from './Card.module.css';
+
+interface CardProps {
+  href: string;
+  title: string;
+  body: string;
+}
+
+export const Card: FC<CardProps> = ({ href, title, body }) => {
+  return (
+    <a href={href} className={styles.card}>
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </a>
+  );
+};
